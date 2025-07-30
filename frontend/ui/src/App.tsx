539dd47083015b583c82e2dc6af9756a87b7c6ce
Damarrion Morgan Harper <dgodsonmo@gmail.com>
2025-07-30 12:39:13 -0700

    Add theme provider and update color variables
    Introduces a ThemeProvider component for managing light, dark, and system themes. Removes App.css, updates App.tsx to wrap content in ThemeProvider, and revises color variables in index.css for improved theming consistency.

--- frontend/ui/src/App.tsx
+++ frontend/ui/src/App.tsx
@@ -1,13 +1,15 @@
 import Index from "./pages/Index"
+import { ThemeProvider } from "./components/theme/themeprovider"
 
 function App() {
 
 
   return (
-    <>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <Index />
-    </>
+    </ThemeProvider>
   )
 }
 
 export default App
+
